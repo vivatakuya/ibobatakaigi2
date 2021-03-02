43d0012bd4ffa023554a71889bf6b96e01a3ffa4
大沢卓也 <vivatomoya0611@gmail.com>
2021-03-03 06:24:08 +0900

    set your name

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,52 +1,56 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import Link from '@material-ui/core/Link';
-import Paper from '@material-ui/core/Paper';
-import Box from '@material-ui/core/Box';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+import React, {　useEffect, useState } from "react";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Link from "@material-ui/core/Link";
+import Paper from "@material-ui/core/Paper";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://twitter.com/home?lang=ja"
-      target="brank"
-      rel="noopener">
+      {"Copyright © "}
+      <Link
+        color="inherit"
+        href="https://twitter.com/home?lang=ja"
+        target="brank"
+        rel="noopener"
+      >
         卓也
-      </Link>{' '}
-      
+      </Link>{" "}
     </Typography>
   );
 }
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    height: '100vh',
+    height: "100vh",
   },
   image: {
-    backgroundImage: 'url(https://source.unsplash.com/random)',
-    backgroundRepeat: 'no-repeat',
+    backgroundImage: "url(https://source.unsplash.com/random)",
+    backgroundRepeat: "no-repeat",
     backgroundColor:
-      theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
+      theme.palette.type === "light"
+        ? theme.palette.grey[50]
+        : theme.palette.grey[900],
+    backgroundSize: "cover",
+    backgroundPosition: "center",
   },
   paper: {
     margin: theme.spacing(8, 4),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main,
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
+    width: "100%", // Fix IE 11 issue.
     marginTop: theme.spacing(1),
   },
   submit: {
@@ -54,8 +58,16 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function SignInSide() {
+export default function SignInSide({ setName }) {
   const classes = useStyles();
+  const[disabled,setDisabled]= useState(true);
+  const[string,setString]= useState('');
+  console.log(disabled, string);
+
+  useEffect(()=>{
+    const disabled = string === ''
+    setDisabled(disabled)
+  },[string]);
 
   return (
     <Grid container component="main" className={classes.root}>
@@ -63,7 +75,6 @@ export default function SignInSide() {
       <Grid item xs={false} sm={4} md={7} className={classes.image} />
       <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
         <div className={classes.paper}>
-          
           <Typography component="h1" variant="h5">
             ようこそ
           </Typography>
@@ -77,19 +88,30 @@ export default function SignInSide() {
               label="ニックネーム"
               name="name"
               autoFocus
+              onChange={(e)=>setString(e.target.value)}
+              onKeyDown={(e)=>{
+               
+                if(e.key === 'Enter'){
+                  setName(e.target.value);
+                e.preventDefault();
+                }
+              }}
             />
-            
-            
+
             <Button
-              type="submit"
+              type="button"
               fullWidth
               variant="contained"
               color="primary"
               className={classes.submit}
+              disabled={disabled}
+              onClick={()=>{
+                setName(string)
+              }}
             >
               はじめる
             </Button>
-            
+
             <Box mt={5}>
               <Copyright />
             </Box>
