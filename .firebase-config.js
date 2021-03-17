5bf390b35e2cb8dce38a12ce8d31dc0a092272e9
大沢卓也 <vivatomoya0611@gmail.com>
2021-03-18 05:20:54 +0900

    create-env-local

---
+++ .firebase-config.js
@@ -0,0 +1,9 @@
+module.exports ={
+    apiKey: "AIzaSyDm0ghpiqsLy9tSrbXj2xGc6os3Unt1GiI",
+    authDomain: "ibobatakaigi-with-to.firebaseapp.com",
+    databaseURL: "https://ibobatakaigi-with-to-default-rtdb.firebaseio.com",
+    projectId: "ibobatakaigi-with-to",
+    storageBucket: "ibobatakaigi-with-to.appspot.com",
+    messagingSenderId: "56346847743",
+    appId: "1:56346847743:web:fa4695e501e9285f169fca"
+  };
--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import {List,ListItem} from '@material-ui/core'
+import {List} from '@material-ui/core'
 import { makeStyles } from "@material-ui/core/styles";
 
 import MessageItem from "./MessageItem"
